f34d51813492850ed28bf60d8f17eb2641bcb54d
niko456gt <niko456gt@gmail.com>
2024-06-29 22:09:35 -0500

    first commit

---
+++ crm/webapp/forms.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms 
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
+from .models import Record
+
+#class for create user
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta: 
+        model = User
+        fields = ['username','password1','password2']
+
+#login class
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+    
+
+
+class CreateRecordForm(forms.ModelForm):
+    class Meta: 
+        model = Record
+        fields = ['first_name','last_name','email','phone','address','city','state','country']
+
+
+class UpdateRecordForm(forms.ModelForm):
+    class Meta: 
+        model = Record
+        fields = ['first_name','last_name','email','phone','address','city','state','country']
---
+++ crm/webapp/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
+
+from django.contrib.auth.models import auth 
+from django.contrib.auth import authenticate
+
+
+from django.contrib.auth.decorators import login_required
+
+
+from .models import Record
+
+# Create your views here.
+
+
+def home(request):
+
+    return render (request, 'webapp/index.html' )
+
+
+
+
+# Register
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('my-login')
+
+    context = {'form':form}
+    return render(request, 'webapp/register.html', context=context)
+
+
+
+# login menu
+def my_login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+
+    context = {'form2':form}
+    return render(request, 'webapp/my-login.html', context= context)
+
+# logout 
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect('my-login')
+
+# main menu
+@login_required(login_url='my-login')
+def dashboard(request):
+
+    my_record = Record.objects.all()
+    context = {'records': my_record}
+    return render(request,'webapp/dashboard.html', context = context)
+
+
+# record add 
+@login_required(login_url='my-login')
+def create_record(request):
+    form = CreateRecordForm()
+    if request.method == "POST":
+        form = CreateRecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard")
+    context = {'form3':form}
+    return render(request, 'webapp/create-record.html',context=context)
+
